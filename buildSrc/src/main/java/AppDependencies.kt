019a994a8c77f61f974189b5cf491156065e275e
ilya <ilya68rus01@gmail.com>
2021-06-14 16:26:07 +0300

    Replace groovy on kotlin DSL but have error
     Duplicate class org.intellij.lang.annotations.Identifier found in modules jetified-annotations-12.0 (com.intellij:annotations:12.0) and jetified-annotations-13.0 (org.jetbrains:annotations:13.0)

---
+++ buildSrc/src/main/java/AppDependencies.kt
@@ -0,0 +1,124 @@
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+object AppDependencies {
+    //std lib
+    val stdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+
+    //android ui
+    val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+
+    val cardView = "androidx.cardview:cardview:1.0.0"
+    val material = "com.google.android.material:material:1.3.0"
+
+
+    //Navigation component
+    // Kotlin
+    val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    val navigationUiKtx = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+
+    // Feature module Support
+    val navigationFeatures =
+        "androidx.navigation:navigation-dynamic-features-fragment:${Versions.navigation}"
+
+    // Jetpack Compose Integration
+    val navigationCompose = "androidx.navigation:navigation-compose:2.4.0-alpha01"
+
+    //Paging Library
+    val paging = "androidx.paging:paging-runtime:${Versions.paging}"
+
+    //RxJava3 support
+    val pagingRx = "androidx.paging:paging-rxjava3:${Versions.paging}"
+
+    //Jetpack Compose integration
+    val pagingCompose = "androidx.paging:paging-compose:1.0.0-alpha09"
+
+    //RxJava 3
+    val rxAndroid = "io.reactivex.rxjava3:rxandroid:3.0.0"
+    val rxJava = "io.reactivex.rxjava3:rxjava:3.0.0"
+
+    //Room
+    val room = "androidx.room:room-runtime:${Versions.room}"
+    val roomAnnotation = "androidx.room:room-compiler:${Versions.room}"
+
+    // optional - RxJava3 support for Room
+    val roomRx = "androidx.room:room-rxjava3:${Versions.room}"
+
+    //Retrofit
+    val retrofit = "com.squareup.retrofit2:retrofit:2.1.0"
+
+    // Kotlin
+    val fragmentKtx = "androidx.fragment:fragment-ktx:${Versions.fragment}"
+
+
+    //test libs
+    val junit = "junit:junit:${Versions.junit}"
+    val extJUnit = "androidx.test.ext:junit:${Versions.extJunit}"
+    val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+
+    // Testing Fragments in Isolation
+    val fragmetnTesting = "androidx.fragment:fragment-testing:${Versions.fragment}"
+
+    // Testing Navigation
+    val navigationTesting = "androidx.navigation:navigation-testing:${Versions.navigation}"
+
+    val appLibraries = arrayListOf<String>().apply {
+        add(stdLib)
+        add(coreKtx)
+        add(appcompat)
+        add(constraintLayout)
+        add(cardView)
+        add(material)
+        add(navigationFragment)
+        add(navigationUiKtx)
+        add(navigationFeatures)
+        add(navigationCompose)
+        add(paging)
+        add(pagingRx)
+        add(pagingCompose)
+        add(rxAndroid)
+        add(rxJava)
+        add(room)
+        add(roomAnnotation)
+        add(roomRx)
+        add(retrofit)
+        add(fragmentKtx)
+    }
+
+    val androidTestLibraries = arrayListOf<String>().apply {
+        add(extJUnit)
+        add(espressoCore)
+        add(navigationTesting)
+        add(fragmetnTesting)
+    }
+
+    val testLibraries = arrayListOf<String>().apply {
+        add(junit)
+    }
+}
+
+//util functions for adding the different type dependencies from build.gradle file
+fun DependencyHandler.kapt(list: List<String>) {
+    list.forEach { dependency ->
+        add("kapt", dependency)
+    }
+}
+
+fun DependencyHandler.implementation(list: List<String>) {
+    list.forEach { dependency ->
+        add("implementation", dependency)
+    }
+}
+
+fun DependencyHandler.androidTestImplementation(list: List<String>) {
+    list.forEach { dependency ->
+        add("androidTestImplementation", dependency)
+    }
+}
+
+fun DependencyHandler.testImplementation(list: List<String>) {
+    list.forEach { dependency ->
+        add("testImplementation", dependency)
+    }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,21 @@
+//version constants for the Kotlin DSL dependencies
+object Versions {
+    //app level
+    const val gradle = "4.2.0"
+    const val kotlin = "1.5.10"
+
+    //libs
+    val coreKtx = "1.3.2"
+    val appcompat = "1.2.0"
+    val constraintLayout = "2.0.4"
+    val dagger = "2.37"
+    val navigation = "2.3.5"
+    val paging = "3.0.0"
+    val room = "2.3.0"
+    val fragment = "1.3.4"
+
+    //test
+    val junit = "4.12"
+    val extJunit = "1.1.2"
+    val espresso = "3.3.0"
+}
