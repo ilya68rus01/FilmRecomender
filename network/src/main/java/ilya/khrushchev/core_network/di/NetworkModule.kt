12899983fc9a23ceee6209775f2915f0c92750e3
ilya <ilya68rus01@gmail.com>
2021-06-20 16:06:00 +0300

    Add di for network module

---
+++ network/src/main/java/ilya/khrushchev/core_network/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package ilya.khrushchev.core_network.di
+
+import dagger.Module
+import dagger.Provides
+import ilya.khrushchev.core_network.TMDbRequests
+import ilya.khrushchev.core_network.TMDbApi
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Retrofit
+
+private val TMDB_BASE_URL = "https://api.themoviedb.org/3"
+
+@Module
+class NetworkModule {
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun provideCallAdapterFactory(): RxJava3CallAdapterFactory {
+        return RxJava3CallAdapterFactory.create()
+    }
+
+    @Provides
+    fun provideTMDbRepository(api: TMDbApi): TMDbRequests {
+        return TMDbRequests(api)
+    }
+
+    @Provides
+    fun providesOpenWeatherRetrofit(
+        converterFactory: GsonConverterFactory,
+        callAdapter: RxJava3CallAdapterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(TMDB_BASE_URL)
+            .addCallAdapterFactory(callAdapter)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+
+    @Provides
+    fun provideTMDbApi(retrofit: Retrofit): TMDbApi {
+        return retrofit.create(TMDbApi::class.java)
+    }
+}
---
+++ network/src/main/java/ilya/khrushchev/core_network/di/RetrofitComponent.kt
@@ -0,0 +1,7 @@
+package ilya.khrushchev.core_network.di
+
+import dagger.Component
+
+@Component(modules = [NetworkModule::class])
+class RetrofitComponent {
+}
