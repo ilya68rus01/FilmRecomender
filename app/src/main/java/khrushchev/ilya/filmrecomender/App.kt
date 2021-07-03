9a9c3356e10bc953ed5d924cbc0bdbabc8eae4af
ilya <ilya68rus01@gmail.com>
2021-07-03 16:10:28 +0300

    Create dependency graph

--- app/src/main/java/khrushchev/ilya/filmrecomender/App.kt
+++
@@ -1,6 +0,0 @@
-package khrushchev.ilya.filmrecomender
-
-import android.app.Application
-
-class App : Application() {
-}
--- app/src/main/java/khrushchev/ilya/filmrecomender/di/components/ApplicationComponent.kt
+++ app/src/main/java/khrushchev/ilya/filmrecomender/di/components/ApplicationComponent.kt
@@ -1,11 +1,11 @@
 package khrushchev.ilya.filmrecomender.di.components
 
-import com.example.core.App
 import com.example.core.di.ApplicationProvider
 import com.example.core.di.MainToolsProvider
 import com.example.core.di.RepoProvider
 import com.example.repo.di.RepoComponent
 import dagger.Component
+import khrushchev.ilya.filmrecomender.Application
 import khrushchev.ilya.filmrecomender.di.ApplicationScope
 import khrushchev.ilya.filmrecomender.di.modules.ViewModelModule
 
@@ -18,17 +18,22 @@ import khrushchev.ilya.filmrecomender.di.modules.ViewModelModule
     ]
 )
 interface ApplicationComponent : ApplicationProvider {
-    fun inject(app: App)
+    fun inject(app: Application)
 
     class Initializer private constructor() {
         companion object {
 
-            fun init(app: App) {
+            fun init(app: Application): ApplicationComponent {
                 val mainToolsProvider = MainToolsComponent.Initializer
                     .init(app)
 
                 val repoProvider = RepoComponent.Initializer
                     .init(mainToolsProvider)
+
+                return DaggerApplicationComponent.builder()
+                    .mainToolsProvider(mainToolsProvider)
+                    .repoProvider(repoProvider)
+                    .build()
             }
         }
     }
