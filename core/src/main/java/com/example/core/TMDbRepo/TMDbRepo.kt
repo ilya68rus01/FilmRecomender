73bcbf4e28c05977538d930414104adc1e7895b2
ilya <ilya68rus01@gmail.com>
2021-07-04 17:27:56 +0300

    Update requests

--- core/src/main/java/com/example/core/TMDbRepo/TMDbRepo.kt
+++ core/src/main/java/com/example/core/TMDbRepo/TMDbRepo.kt
@@ -1,5 +1,9 @@
 package com.example.core.TMDbRepo
 
+import com.example.core.data.Film
+import com.example.core.data.Page
+import io.reactivex.rxjava3.core.Single
+
 interface TMDbRepo {
-    fun getNewFilms(page: Int)
+    fun getNewFilms(page: Int): Single<Page<Film>>
 }
