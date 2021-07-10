41169f8d17ceec7af8cb98834675341d8b7794b6
ilya <ilya68rus01@gmail.com>
2021-07-10 15:07:53 +0300

    Remove snake case

--- network/src/main/java/com/example/network/data/LastEpisodeToAir.kt
+++ network/src/main/java/com/example/network/data/LastEpisodeToAir.kt
@@ -1,14 +1,14 @@
 package com.example.network.data
 
 data class LastEpisodeToAir(
-    val air_date: String,
-    val episode_number: Int,
+    val airDate: String,
+    val episodeNumber: Int,
     val id: Int,
     val name: String,
     val overview: String,
-    val production_code: String,
-    val season_number: Int,
-    val still_path: String?,
-    val vote_average: Float,
-    val vote_count: Int
+    val productionCode: String,
+    val seasonNumber: Int,
+    val stillPath: String?,
+    val voteAverage: Float,
+    val voteCount: Int
 )
