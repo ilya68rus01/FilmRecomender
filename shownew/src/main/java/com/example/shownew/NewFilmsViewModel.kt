dd44efcc872626a271708633091f115929e174a2
ilya <ilya68rus01@gmail.com>
2021-07-17 15:16:20 +0300

    Create variable private

--- shownew/src/main/java/com/example/shownew/NewFilmsViewModel.kt
+++ shownew/src/main/java/com/example/shownew/NewFilmsViewModel.kt
@@ -12,7 +12,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 class NewFilmsViewModel @Inject constructor(
-    val repo: TMDbRepo
+    private val repo: TMDbRepo
 ) : ViewModel() {
 
     private val _newFilmsLiveData = liveData<Page<Film>> {
