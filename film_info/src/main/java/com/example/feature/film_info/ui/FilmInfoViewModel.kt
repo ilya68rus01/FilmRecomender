1dccdc54aeae5147fc13c9118c6c8770c0a428d3
Ilya <ilya68rus01@gmail.com>
2022-06-25 21:42:05 +0300

    Add providing FilmInfoDatasource

--- film_info/src/main/java/com/example/feature/film_info/ui/FilmInfoViewModel.kt
+++ film_info/src/main/java/com/example/feature/film_info/ui/FilmInfoViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.feature.film_info.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.feature.film_info.repository.FilmInfoDataSource
 import javax.inject.Inject
 
 class FilmInfoViewModel @Inject constructor(
-
+    private val filmInfoDataSource: FilmInfoDataSource
 ) : ViewModel() {
+
 }
