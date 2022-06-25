266a00771734896f850fda05a3bd345566ceb051
Ilya <ilya68rus01@gmail.com>
2022-06-25 21:39:34 +0300

    Add extension for getting properties

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,3 +1,6 @@
+import org.gradle.api.Project
+import java.io.File
+
 //app level config constants
 object AppConfig {
     const val compileSdk = 30
@@ -10,4 +13,16 @@ object AppConfig {
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules =  "consumer-rules.pro"
     const val dimension = "environment"
+}
+
+fun Project.getLocalProperty(key: String, file: String = "local.properties"): Any {
+    val properties = java.util.Properties()
+    val localProperties = File(file)
+    if (localProperties.isFile) {
+        java.io.InputStreamReader(java.io.FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+            properties.load(reader)
+        }
+    } else error("File from not found")
+
+    return properties.getProperty(key)
 }
